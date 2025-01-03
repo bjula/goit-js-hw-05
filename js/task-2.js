a94d942e7e389ae1f7a882607b84d9e573eb75e0
Yuliia Besedina <92574244+bjula@users.noreply.github.com>
2025-01-03 18:50:57 +0200

    update task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,7 @@
-const getUsersWithFriend = (users, friendName) => {
-    return users
-        .filter(user => user.friends.includes(friendName));
+const getUsersWithFriend = (users, friendName) =>
+    users
+      .filter(user => user.friends.includes(friendName));
     
-};
 const allUsers = [
   {
     name: "Moore Hensley",
